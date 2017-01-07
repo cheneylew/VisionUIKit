dff082956b454a63f9fec703fc533f52e38dff62
Deju Liu <cheneylew@163.com>
2017-01-07 18:23:25 +0800

    更新组件0.1.36

--- VisionUIKit/Classes/Categories/NSBundle+VisionUIKit.m
+++ VisionUIKit/Classes/Categories/NSBundle+VisionUIKit.m
@@ -16,13 +16,17 @@
     static NSBundle *sourceBundle = nil;
     if (sourceBundle == nil) {
         // 这里不使用mainBundle是为了适配pod 1.x和0.x
-        sourceBundle = [self vs_bundleName:@"VisionUIKit"];
+        sourceBundle = [self vs_bundleName:@"VisionUIKit" forClass:[self class]];
     }
     return sourceBundle;
 }
 
 + (NSBundle *)vs_bundleName:(NSString *) name {
     return [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:name ofType:@"bundle"]];
+}
+
++ (NSBundle *)vs_bundleName:(NSString *) name forClass:(Class) class {
+    return [NSBundle bundleWithPath:[[NSBundle bundleForClass:class] pathForResource:name ofType:@"bundle"]];
 }
 
 
